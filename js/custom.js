e79bd7900581d5b55e5fc952c9bd3ea6fc0799f8
engrasel <contact.engrasel@gmail.com>
2021-07-25 13:54:32 +0600

    commit

--- js/custom.js
+++ js/custom.js
@@ -7,4 +7,51 @@
     offcanvasToggle.addEventListener("click", function () {
       offcanvasCollapse.classList.toggle("open");
     });
-  })();
+})();
+  
+// slick slider
+$('.trust-logo-deck').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  rows: 1,
+  slidesPerRow: 1,
+  autoplay: true,
+  dots: false,
+  prevArrow: $('.trust-container .prev'),
+  nextArrow: $('.trust-container .next'),
+  mobileFirst: true,
+  responsive: [{
+          breakpoint: 575,
+          settings: {
+              rows: 1,
+              slidesToShow: 2,
+              slidesToScroll: 1
+          }
+      },
+      {
+          breakpoint: 991,
+          settings: {
+              rows: 1,
+              slidesToShow: 4,
+              slidesToScroll: 1
+          }
+      },
+      {
+          breakpoint: 1200,
+          settings: {
+              rows: 1,
+              slidesToShow: 4,
+              slidesToScroll: 1
+          }
+      },
+      {
+          breakpoint: 1600,
+          settings: {
+              rows: 1,
+              slidesToShow: 5,
+              slidesToScroll: 1
+          }
+      }
+  ]
+});
